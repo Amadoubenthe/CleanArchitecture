77ce049a0f4d572890e9b3f92136205d1fbe4109
Amadoubenthe <amadbenthebagnandiallo@gmail.com>
2024-12-04 13:45:14 +0000

    Add Domain layer to project

---
+++ CleanArchitecture.Domain/Interfaces/IBlogRepository.cs
@@ -0,0 +1,13 @@
+﻿using CleanArchitecture.Domain.Entites;
+
+namespace CleanArchitecture.Domain.Interfaces
+{
+    public interface IBlogRepository
+    {
+        Task<List<Blog>> GetAllAsync();
+        Task<Blog> GetByIdAsync(int id);
+        Task<Blog> AddAsync(Blog blog);
+        Task<Blog> UpdateAsync(int id, Blog blog);
+        Task<Blog> DeleteAsync(int id);
+    }
+}
