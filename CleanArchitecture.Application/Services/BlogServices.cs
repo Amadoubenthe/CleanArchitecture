c40fcdfe8ff5abd9472ae6a736b2a79ce06983fe
Amadoubenthe <amadbenthebagnandiallo@gmail.com>
2024-12-04 20:06:11 +0000

    Add Application Layer

---
+++ CleanArchitecture.Application/Services/BlogServices.cs
@@ -0,0 +1,39 @@
+﻿using CleanArchitecture.Domain.Entites;
+using CleanArchitecture.Domain.Interfaces;
+
+namespace CleanArchitecture.Application.Services
+{
+    public class BlogServices : IBlogServices
+    {
+        public IBlogRepository _blogRepository { get; }
+        public BlogServices(IBlogRepository blogRepository)
+        {
+            _blogRepository = blogRepository;
+        }
+
+        public async Task<Blog> AddAsync(Blog blog)
+        {
+            return await _blogRepository.AddAsync(blog);
+        }
+
+        public async Task<Blog> DeleteAsync(int id)
+        {
+            return await _blogRepository.DeleteAsync(id);
+        }
+
+        public async Task<List<Blog>> GetAllAsync()
+        {
+            return await _blogRepository.GetAllAsync();
+        }
+
+        public async Task<Blog> GetByIdAsync(int id)
+        {
+            return await _blogRepository.GetByIdAsync(id);
+        }
+
+        public async Task<Blog> UpdateAsync(int id, Blog blog)
+        {
+            return await _blogRepository.UpdateAsync(id, blog);
+        }
+    }
+}
---
+++ CleanArchitecture.Application/Services/IBlogServices.cs
@@ -0,0 +1,13 @@
+﻿using CleanArchitecture.Domain.Entites;
+
+namespace CleanArchitecture.Application.Services
+{
+    public interface IBlogServices
+    {
+        Task<List<Blog>> GetAllAsync();
+        Task<Blog> GetByIdAsync(int id);
+        Task<Blog> AddAsync(Blog blog);
+        Task<Blog> UpdateAsync(int id, Blog blog);
+        Task<Blog> DeleteAsync(int id);
+    }
+}
